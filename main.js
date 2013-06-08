ca5859def29a3a0f2cec4f38e4af8077c891ef5d
Ivan Chub <subsbox@hotmail.com>
2013-06-08 11:29:05 -0700

    made drawing work, added a bunch of comments

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ jQuery(function($) {
 	// does not affect the color calculations
 	var spacing = 0;
 
+	// if true, then the x and y parameters of the color
+	// calculation functions will be the actual position of 
+	// the square. otherwise, the number of the point
+	// will be passed - its number horizontaly and vertically
+	var useActualCoordinates = true;
+
 	// size of each point in pixels
 	var pointDimensions = {
 		X: 10,
@@ -16,58 +22,73 @@ jQuery(function($) {
 
 	// dimensions are in amount of pixels
 	var displayDimensions = {
-		X: 10,
-		Y: 10,
+		X: 100,
+		Y: 50,
 	}
-
-	// each individual color of points
-	var points = [];
-
-	
 	
 	// resizes the canvas so it fits the whole screen
-	var updateCanvasSize = function() {
+	var onResize = function() {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
+		displayDimensions.X = window.innerWidth / (pointDimensions.X - 1);
+		displayDimensions.Y = window.innerHeight / (pointDimensions.Y - 1);
+		draw();
 	};
 
-	// initializes the canvas
-	var init = function() {
-		updateCanvasSize();
+	// converts an rgb value to a string that can be used
+	// to style each point
+	var RGB = function (R, G, B) {
+		return "#" + R.toString(16) + G.toString(16) + B.toString(16);
+	};	
 
-		for (var i = 0; i < displayDimensions.X; i++) {
-			points[i] = []
-			for (var j = 0; j < displayDimensions.Y; j++) {
-				points[i][j] = "#FFFFFF";
-			}
+	var calcR = function (x, y) {
+		return Math.sin(x) * 2;
+	};
+
+	var calcG = function (x, y) {
+		return Math.cos(y) * 2;
+	};
+
+	var calcB = function (x, y) {
+		return Math.tan(x + y) * 2;
+	};
+
+	var calcColor = function (x, y) {
+		var r = Math.floor(calcR(x, y)) % 256;
+		var g = Math.floor(calcG(x, y)) % 256;
+		var b = Math.floor(calcB(x, y)) % 256;
+
+		if (r == undefined | g == undefined | b == undefined) {
+			return RGB(100, 100, 100);
 		}
+		else {
+			return RGB(r, g, b);
+		}
+	}
 
-		setInterval(function() {
-			update();
-			draw();
-		},
-		10);
-	};
-
-	// updates each pixel
-	var update = function() {
-		
-	};
-
-	var draw = function() {
+	var draw = function () {
 		for (var i = 0; i < displayDimensions.X; i++) {
 			for (var j = 0; j < displayDimensions.Y; j++) {
-				ctx.fillRect(i * (pointDimensions.X + spacing),
-						j * (pointDimensions.Y + spacing), 
+
+				var x = i * (pointDimensions.X + spacing);
+				var y = j * (pointDimensions.Y + spacing);
+				if (useActualCoordinates) {
+					ctx.fillStyle = calcColor(x, y);
+				}
+				else {
+					ctx.fillStyle = calcColor(i, j);
+				}
+
+				ctx.fillRect(
+						x,
+						y, 
 						pointDimensions.X,
 					   	pointDimensions.Y);
 			}
 		}
 	};
 
-	$(window).resize(function() {
-		updateCanvasSize();
-	});
+	$(window).resize(onResize);
 
-	init();
+	onResize();
 });
